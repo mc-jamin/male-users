c719295560da53f4b6f2d5d974e04901fd83e250
Jamin <mc.jamin007@gmail.com>
2023-02-19 10:02:52 +0300

    Add files via upload

---
+++ male_users.py
@@ -0,0 +1,18 @@
+#Indtalling the requests module using pip
+#Then importing the requets module
+
+import requests
+
+count=0
+url="https://randomuser.me/api"
+users=[]
+
+if len(users)<100:
+    res= requests.get(url).json()
+    gender = res["results"][0]["gender"]
+    if gender =="male":
+        users.append(res)
+    count = count + 1
+    url = f"https://randomuser.me/api/?page={count}"
+for male in users:
+    print (male)
